0fa8b98d8a481829359a4af3921c5984cb6a40e2
Hoistyr <hoistyd@gmail.com>
2021-02-24 15:26:48 -0700

    Added more functions to buttons, updated design slightly, started button click filtering

--- scripts.js
+++ scripts.js
@@ -76,9 +76,20 @@ function factorial(num1) {
 }
 //Adds event listeners to the buttons on page load
 function calcInit () {
-    getNumbers();
+    loadButtons();
+}
+
+//Initates all the button event listeners on call
+function loadButtons() {
+	getNumbers();
     getOperators();
     getExecuteOperation();
+	getClearButton();
+	getRemoveLastButton();
+	getOpenParenthesisButton();
+	getCloseParenthesisButton();
+	getSquareButton();
+	getSquareRootButton();
 }
 
 //Finds all the buttons with the numbers class
@@ -96,13 +107,55 @@ function getNumbers() {
 function getOperators () {
     let operators = document.getElementsByClassName('operator');
     for(i=0; i < operators.length; i++) {
-        operators[i].addEventListener('click', testAlert);
+        operators[i].addEventListener('click', operatorClicked);
     }
 }
 //Finds the execute operation button
 function getExecuteOperation () {
     let executeOperation = document.getElementById('executeOperation');
     executeOperation.addEventListener('click', testAlert);
+}
+
+//Finds the clear button
+function getClearButton () {
+    let clearButton = document.getElementById('clearButton');
+    clearButton.addEventListener('click', clearClicked);
+}
+
+//Finds the removeLast button
+function getRemoveLastButton () {
+	let removeLastButton = document.getElementById('removeLast');
+    removeLastButton.addEventListener('click', removeLast);
+}
+
+//Finds the open parenthesis button
+function getOpenParenthesisButton () {
+	let openParenthesisButton = document.getElementById('openParenthesis');
+    openParenthesisButton.addEventListener('click', openParenthesisClicked);
+}
+
+//Finds the close parenthesis button
+function getCloseParenthesisButton () {
+	let closeParenthesisButton = document.getElementById('closeParenthesis');
+    closeParenthesisButton.addEventListener('click', closeParenthesisClicked);
+}
+
+//Finds the decimal button
+function getDecimalButton () {
+	let decimalButton = document.getElementById('closeParenthesis');
+   decimalButton.addEventListener('click', decimalClicked);
+}
+
+//Finds the square button
+function getSquareButton () {
+	let squareButton = document.getElementById('square');
+   	squareButton.addEventListener('click', squareClicked);
+}
+
+//Finds the square root button
+function getSquareRootButton () {
+	let squareRootButton = document.getElementById('squareRoot');
+   	squareRootButton.addEventListener('click', squareRootClicked);
 }
 
 function testAlert() {
@@ -116,18 +169,184 @@ function numberClicked(eventData) {
     insertDisplay(numberClicked);
 }
 
-//Inserts the number to the display
-function insertDisplay (numberClicked) {
+//Determines which operator was clicked
+function operatorClicked(eventData) {
+	let buttonInformation = eventData;
+	let operatorClicked = buttonInformation.target.textContent;
+	insertDisplay(operatorClicked);
+}
+
+//Inserts the input to the display
+function insertDisplay (input) {
     let displayText = document.getElementById('displayText')
-    let textContent = displayText.textContent;
-    console.log(textContent);
-    if (textContent === '0') {
-        displayText.textContent = numberClicked;
-        textContent = numberClicked;
+	let currentlyOnscreen = displayText.textContent;
+
+    //Checks if the number onscreen is 0
+	if (currentlyOnscreen === '0') {
+        console.log(input);
+		
+		//Prevents inputting operators where they are invalid when the display is empty
+		if (input !== '+' && input !== '*' && input !== '÷' && input !== ')' && input !== '^2') {
+			displayText.textContent = input;
+			//currentlyOnscreen = displayText.textContent;
+		}
+	
+	
     } else {
         console.log('in else');
-        
-        displayText.textContent = textContent + numberClicked;
-    }
+		console.log(input);
+        //Stops inputting of operators if the textContent is only a subtraction symbol
+		let onscreenLength = currentlyOnscreen.length
+		let lastChar = currentlyOnscreen.charAt(onscreenLength - 1);
+		let last2Chars = currentlyOnscreen.charAt(onscreenLength - 2) + lastChar;
+		
+		//Only allows close parenthesis where valid
+		if (input === ')') {
+			console.log(currentlyOnscreen);
+			if (currentlyOnscreen.includes('(')) {
+				displayText.textContent = currentlyOnscreen + input;
+			} else if (!currentlyOnscreen.includes('(')) {
+				input = '';
+				displayText.textContent = currentlyOnscreen + input;
+			}
+		}
+
+		//Only allows square to be input where valid
+		if (input === '^2') {
+			
+			console.log(last2Chars);
+			if (
+				last2Chars !== '^2' 
+				&& lastChar !== '('
+				&& lastChar !== '÷'
+				&& lastChar !== '*'
+				&& lastChar !== '-'
+				&& lastChar !== '+'
+				){
+				displayText.textContent = currentlyOnscreen + input;
+			} else {
+				input = '';
+				displayText.textContent = currentlyOnscreen + input;
+			}	
+		}
+		 
+	//Only allows square root where valid
+		if (input === '√(') {
+			if ( lastChar !== '.'){
+				displayText.textContent = currentlyOnscreen + input;
+			} else {
+				input = '';
+				displayText.textContent = currentlyOnscreen + input;
+			}	
+		}
+		
+		//Only allows division where valid
+		if (input === '÷') {
+			if ( 
+				lastChar !== '.'
+				&& lastChar !== '('
+				&& lastChar !== '÷'
+				&& lastChar !== '*'
+				&& lastChar !== '-'
+				&& lastChar !== '+'
+				){
+				displayText.textContent = currentlyOnscreen + input;
+			} else {
+				input = '';
+				displayText.textContent = currentlyOnscreen + input;
+			}	
+		}
+		
+		if (input === '-') {
+			if (lastChar !== '-') {
+				displayText.textContent = currentlyOnscreen + input;
+			}	
+		} 
+			
+		
+		displayText.textContent = currentlyOnscreen + input;
+		}
+		
+		
+		
+		
+		
+		
+		/*(currentlyOnscreen.charAt(0) === '-' && (currentlyOnscreen.charAt(onscreenLength - 1) === '-' ) && (input === '-' || input === '+' || input === '*' || input === '÷' || input === ')' || input === '^2')) {
+			input = '';
+			displayText.textContent = currentlyOnscreen + input;
+		} else {
+			displayText.textContent = currentlyOnscreen + input;
+		}
+		
+		
+		if (currentlyOnscreen.charAt(onscreenLength - 1) === '-' && input === '+' || input === '*' || input === '÷' || input === ')') {
+			input = '';
+			displayText.textContent = currentlyOnscreen + input;
+		
+		} else if (currentlyOnscreen.charAt(onscreenLength - 1) === '-' && currentlyOnscreen.charAt(onscreenLength - 2) === '-' && input === '+' || input === '*' || input === '÷' || input === ')' || input === '-') {
+			input = '';
+			displayText.textContent = currentlyOnscreen + input;
+		}
+        displayText.textContent = currentlyOnscreen + input; */
     
+}
+
+//removes the last things added to the display
+function removeLast() {
+	let displayText = document.getElementById('displayText')
+	let textContent = displayText.textContent;
+	let textContentLength = textContent.length;
+	
+	if (textContent !== '') {
+		textContent = textContent.slice(0,(textContentLength - 1));
+		if (textContent) {
+			displayText.textContent = textContent;
+		} else if (!textContent) {
+			textContent = '0';
+			displayText.textContent = '0';
+		}
+	}
+	
+}
+
+//Resets the display back to 0
+function clearClicked() {
+	let displayText = document.getElementById('displayText')
+	displayText.textContent = '0';
+}
+
+//Adds an open parenthesis
+function openParenthesisClicked (eventData) {
+	let buttonInformation = eventData;
+	let openParenthesisClicked = buttonInformation.target.textContent;
+	insertDisplay(openParenthesisClicked);
+}
+
+//Adds a closed parenthesis
+function closeParenthesisClicked (eventData) {
+	let buttonInformation = eventData;
+	let closeParenthesisClicked = buttonInformation.target.textContent;
+	insertDisplay(closeParenthesisClicked);
+}
+
+//Adds a decimal
+function decimalClicked (eventData) {
+	let buttonInformation = eventData;
+	let decimalClicked = buttonInformation.target.textContent;
+	insertDisplay(decimalClicked);
+}
+
+//Adds a square
+function squareClicked (eventData) {
+	let buttonInformation = eventData;
+	let squareClicked = '^2';
+	insertDisplay(squareClicked);
+}
+
+//Adds a square root
+function squareRootClicked (eventData) {
+	let buttonInformation = eventData;
+	let squareRootClicked = buttonInformation.target.textContent + '(';
+	insertDisplay(squareRootClicked);
 }
