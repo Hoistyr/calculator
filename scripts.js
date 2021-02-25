15a326f543b52e3f4391204987dda27019f4e771
Hoistyr <hoistyd@gmail.com>
2021-02-24 21:15:08 -0700

    Finished button input, started operation logic

--- scripts.js
+++ scripts.js
@@ -5,75 +5,8 @@
 //Function called operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers
 //Clear button
 
-
-//Needs to add
 calcInit();
 
-function add () {
-	let value = 0;
-	for (i = (arguments.length - 1); i >=0; i--) {
-		value = value + arguments[i];
-	}
-	
-	return value;
-}
-
-//Needs to subtract
-function subtract () {
-	let value = arguments[0];
-	for (i = 1; (i < arguments.length); i++) {
-		value = value - arguments[i];
-	}
-
-	return value;
-}
-
-//Needs to multiply
-function multiply (array) {
-	let arrayLength = array.length;
-	let value = 1;
-	for (i = 0; (i < arrayLength); i++) {
-		value = value * array[i];
-	}
-	
-	return value;
-}
-
-
-function sum (array) {
-	let arrayLength = array.length;
-	let value = 0;
-	for (i = 0; (i < arrayLength); i++) {
-		value = value + array[i];
-	}
-	
-	return value;
-}
-
-//Raises a number to a power
-function power() {
-	let argumentsLength = arguments.length;
-	let value = arguments[0];
-	for (i = 1; (i < argumentsLength); i++) {
-		value = value ** arguments[i];
-	}
-	
-	return value;
-}
-
-function factorial(num1) {
-	if (num1 === 0) {
-		let value = 1;
-		return value;
-	} else {
-		let value = num1;
-		for (n = num1; n > 1; n--) {
-			value = value * (n - 1);
-		}	
-		return value;
-	}
-	
-}
 //Adds event listeners to the buttons on page load
 function calcInit () {
     loadButtons();
@@ -83,7 +16,7 @@ function calcInit () {
 function loadButtons() {
 	getNumbers();
     getOperators();
-    getExecuteOperation();
+    getExecuteOperationButton();
 	getClearButton();
 	getRemoveLastButton();
 	getOpenParenthesisButton();
@@ -111,9 +44,9 @@ function getOperators () {
     }
 }
 //Finds the execute operation button
-function getExecuteOperation () {
+function getExecuteOperationButton () {
     let executeOperation = document.getElementById('executeOperation');
-    executeOperation.addEventListener('click', testAlert);
+	executeOperation.addEventListener('click', beginExecution);
 }
 
 //Finds the clear button
@@ -183,7 +116,6 @@ function insertDisplay (input) {
 
     //Checks if the number onscreen is 0
 	if (currentlyOnscreen === '0') {
-        console.log(input);
 		
 		//Prevents inputting operators where they are invalid when the display is empty
 		if (input !== '+' && input !== '*' && input !== '÷' && input !== ')' && input !== '^2') {
@@ -193,8 +125,6 @@ function insertDisplay (input) {
 	
 	
     } else {
-        console.log('in else');
-		console.log(input);
         //Stops inputting of operators if the textContent is only a subtraction symbol
 		let onscreenLength = currentlyOnscreen.length
 		let lastChar = currentlyOnscreen.charAt(onscreenLength - 1);
@@ -202,7 +132,6 @@ function insertDisplay (input) {
 		
 		//Only allows close parenthesis where valid
 		if (input === ')') {
-			console.log(currentlyOnscreen);
 			if (currentlyOnscreen.includes('(')) {
 				displayText.textContent = currentlyOnscreen + input;
 			} else if (!currentlyOnscreen.includes('(')) {
@@ -213,8 +142,6 @@ function insertDisplay (input) {
 
 		//Only allows square to be input where valid
 		if (input === '^2') {
-			
-			console.log(last2Chars);
 			if (
 				last2Chars !== '^2' 
 				&& lastChar !== '('
@@ -256,39 +183,70 @@ function insertDisplay (input) {
 				displayText.textContent = currentlyOnscreen + input;
 			}	
 		}
-		
-		if (input === '-') {
-			if (lastChar !== '-') {
+
+		//Only allows multiplication where valid
+		if (input === '*') {
+			if ( 
+				lastChar !== '.'
+				&& lastChar !== '('
+				&& lastChar !== '÷'
+				&& lastChar !== '*'
+				&& lastChar !== '-'
+				&& lastChar !== '+'
+				){
+				displayText.textContent = currentlyOnscreen + input;
+			} else {
+				input = '';
 				displayText.textContent = currentlyOnscreen + input;
 			}	
-		} 
+		}
+		
+		//Only allows subtraction where valid
+		if (input === '-') {
+			if ( 
+				lastChar !== '.'
+				&& lastChar !== '÷'
+				&& lastChar !== '*'
+				&& lastChar !== '-'
+				&& lastChar !== '+'
+				){
+				displayText.textContent = currentlyOnscreen + input;
+			} else {
+				input = '';
+				displayText.textContent = currentlyOnscreen + input;
+			}	
+		}
+
+		//Only allows addition where valid
+		if (input === '+') {
+			if ( 
+				lastChar !== '.'
+				&& lastChar !== '('
+				&& lastChar !== '÷'
+				&& lastChar !== '*'
+				&& lastChar !== '-'
+				&& lastChar !== '+'
+				){
+				displayText.textContent = currentlyOnscreen + input;
+			} else {
+				input = '';
+				displayText.textContent = currentlyOnscreen + input;
+			}	
+		}
+	
+		//!Still adds decimals when not allowed to!
+		if (input === '.') {
+			if (lastChar !== '.'){
+				displayText.textContent = currentlyOnscreen + input;
+			} else {
+				input = '';
+				displayText.textContent = currentlyOnscreen + input;
+			}	
+		}
 			
 		
 		displayText.textContent = currentlyOnscreen + input;
-		}
-		
-		
-		
-		
-		
-		
-		/*(currentlyOnscreen.charAt(0) === '-' && (currentlyOnscreen.charAt(onscreenLength - 1) === '-' ) && (input === '-' || input === '+' || input === '*' || input === '÷' || input === ')' || input === '^2')) {
-			input = '';
-			displayText.textContent = currentlyOnscreen + input;
-		} else {
-			displayText.textContent = currentlyOnscreen + input;
-		}
-		
-		
-		if (currentlyOnscreen.charAt(onscreenLength - 1) === '-' && input === '+' || input === '*' || input === '÷' || input === ')') {
-			input = '';
-			displayText.textContent = currentlyOnscreen + input;
-		
-		} else if (currentlyOnscreen.charAt(onscreenLength - 1) === '-' && currentlyOnscreen.charAt(onscreenLength - 2) === '-' && input === '+' || input === '*' || input === '÷' || input === ')' || input === '-') {
-			input = '';
-			displayText.textContent = currentlyOnscreen + input;
-		}
-        displayText.textContent = currentlyOnscreen + input; */
+	}
     
 }
 
@@ -297,8 +255,15 @@ function removeLast() {
 	let displayText = document.getElementById('displayText')
 	let textContent = displayText.textContent;
 	let textContentLength = textContent.length;
-	
-	if (textContent !== '') {
+	let lastChar = textContent.charAt(textContentLength - 1);
+	let last2Chars = textContent.charAt(textContentLength - 2) + lastChar;
+	if (textContent) {
+		//removes both parts of the square root upon button click
+		if (last2Chars === '√(') {
+			textContent = textContent.slice(0,(textContentLength - 2));
+			displayText.textContent = textContent;
+		}
+		
 		textContent = textContent.slice(0,(textContentLength - 1));
 		if (textContent) {
 			displayText.textContent = textContent;
@@ -349,4 +314,153 @@ function squareRootClicked (eventData) {
 	let buttonInformation = eventData;
 	let squareRootClicked = buttonInformation.target.textContent + '(';
 	insertDisplay(squareRootClicked);
+}
+
+
+//Actual calculating-----------------------------------------
+
+//executes the operation
+function beginExecution() {
+	let currentlyOnscreen = displayText.textContent;
+	executionContinued(currentlyOnscreen);
+}
+
+function executionContinued (input) {
+	let fromDisplay = input;
+	let i = 0;
+	let fromParenthesis = [];
+	
+	if (fromDisplay.includes('(' && ')')) {
+		while (fromDisplay.includes('(' && ')')) {
+			let openIndex = fromDisplay.indexOf('(');
+			let closeIndex = fromDisplay.indexOf(')');
+			let insideParenthesis = fromDisplay.slice(openIndex + 1, closeIndex);
+			fromParenthesis[i] = insideParenthesis;
+			i++;
+			
+			let withoutParenthesis = fromDisplay.slice(closeIndex + 1);
+			let operation = withoutParenthesis.slice(0,1);
+			fromParenthesis[i] = operation;
+			i++;
+			console.log(i);
+			fromDisplay = withoutParenthesis.slice(1);
+			console.log(fromParenthesis);
+		}
+	} else if (fromDisplay.includes('^2') && (!fromDisplay.includes('(' && ')'))) {
+		if (fromDisplay.includes('+' || '-' || '*' || '÷' || '√')) {
+			console.log('extra');
+		} else {
+			let arrowIndex = fromDisplay.indexOf('^');
+			let toBeSquared = fromDisplay.slice(0, arrowIndex);
+			let numSquared = square(toBeSquared);
+			displayText.textContent = numSquared;
+		}
+	} else if (fromDisplay.includes('*')
+		&& (!fromDisplay.includes('(' && ')'))
+		&& (!fromDisplay.includes('^2'))
+		) {
+		if (fromDisplay.includes('+' || '-' || '^2' || '÷' || '√')) {
+			console.log('extra');
+		} else {
+			let multiplyIndex = fromDisplay.indexOf('*');
+			let firstNum = fromDisplay.slice(0, multiplyIndex);
+			let nextNum = fromDisplay.slice(multiplyIndex + 1,)
+			multiplied = firstNum * nextNum;
+			displayText.textContent = multiplied;
+		}
+	} else if (fromDisplay.includes('÷')
+	&& (!fromDisplay.includes('(' && ')'))
+	&& (!fromDisplay.includes('^2'))
+	) {
+	if (fromDisplay.includes('+' || '-' || '^2' || '*' || '√')) {
+		console.log('extra');
+	} else {
+		let multiplyIndex = fromDisplay.indexOf('*');
+		let firstNum = fromDisplay.slice(0, multiplyIndex);
+		let nextNum = fromDisplay.slice(multiplyIndex + 1,)
+		multiplied = firstNum * nextNum;
+		displayText.textContent = multiplied;
+	}
+	
+	for (i = 0; i < fromParenthesis.length; i++ ) {
+		//(!fromParenthesis[i].includes('^2'))
+		console.log(fromParenthesis[i]);
+	}
+	
+	i = 0;
+}
+	
+	
+	/*if (!fromParenthesis.includes('(' && ')')) {
+		while (fromParenthesis[i].includes('*')) {
+			let multiplyIndex = fromParenthesis[i].indexOf('*')
+			console.log(multiplyIndex);
+			return;
+		}
+	} */
+	//while (fromParenthesis includes
+}
+
+function findParenthesis () {
+
+}
+
+//Needs to add
+function add () {
+	let value = 0;
+	for (i = (arguments.length - 1); i >=0; i--) {
+		value = value + arguments[i];
+	}
+	
+	return value;
+}
+
+//Needs to subtract
+function subtract () {
+	let value = arguments[0];
+	for (i = 1; (i < arguments.length); i++) {
+		value = value - arguments[i];
+	}
+
+	return value;
+}
+
+//Needs to multiply
+function multiply (array) {
+	let arrayLength = array.length;
+	let value = 1;
+	for (i = 0; (i < arrayLength); i++) {
+		value = value * array[i];
+	}
+	
+	return value;
+}
+
+//Squares a number
+function square() {
+	let value = arguments[0];
+	value = value ** 2;
+	
+	return value;
+}
+
+//Raises a number to a power
+function power() {
+	let argumentsLength = arguments.length;
+	let value = arguments[0];
+	for (i = 1; (i < argumentsLength); i++) {
+		value = value ** arguments[i];
+	}
+	
+	return value;
+}
+
+function sum (array) {
+	let arrayLength = array.length;
+	let value = 0;
+	for (i = 0; (i < arrayLength); i++) {
+		value = value + array[i];
+	}
+	
+	return value;
 }
