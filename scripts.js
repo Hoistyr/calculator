5424d0d0d21e9df844a585917ffc1e35adf93537
Hoistyr <hoistyd@gmail.com>
2021-02-25 17:44:50 -0700

    Continued work on operation functions, added some compatability to the square function

--- scripts.js
+++ scripts.js
@@ -426,10 +426,37 @@ function squareRoot (fromDisplay) {
 			let openIndex = fromDisplay.indexOf('(', rootIndex);
 			let closeIndex = fromDisplay.indexOf(')', rootIndex);
 			let insideSquareParenthesis = fromDisplay.slice(openIndex + 1, closeIndex);
-			squareRooted = Math.sqrt(insideSquareParenthesis);
-			displayText.textContent = squareRooted;
-			fromDisplay = fromDisplay.slice(0,rootIndex) + squareRooted + fromDisplay.slice(closeIndex + 1);
-			return fromDisplay;
+			console.log(insideSquareParenthesis);
+			
+			if (insideSquareParenthesis.includes('+')
+			 || insideSquareParenthesis.includes('-')
+			 || insideSquareParenthesis.includes('*')
+			 || insideSquareParenthesis.includes( '√')
+			 || insideSquareParenthesis.includes('÷') 
+			 ||insideSquareParenthesis.includes('^') 
+			 ) {
+				console.log('inside dif sqrrt');
+				toBeSquareRooted = square(insideSquareParenthesis);
+				console.log(toBeSquareRooted);
+				toBeSquareRooted = multiply(toBeSquareRooted);
+				console.log(toBeSquareRooted);
+				toBeSquareRooted = add(toBeSquareRooted);
+				console.log(toBeSquareRooted);
+				toBeSquareRooted = subtract(toBeSquareRooted);
+				console.log(toBeSquareRooted);
+				squareRooted = Math.sqrt(toBeSquareRooted);
+				displayText.textContent = squareRooted;
+				fromDisplay = fromDisplay.slice(0,rootIndex) + squareRooted + fromDisplay.slice(closeIndex + 1);
+				return fromDisplay
+
+			} else {
+				console.log('did not contain other operations');
+				squareRooted = Math.sqrt(insideSquareParenthesis);
+				displayText.textContent = squareRooted;
+				fromDisplay = fromDisplay.slice(0,rootIndex) + squareRooted + fromDisplay.slice(closeIndex + 1);
+				return fromDisplay;
+			}
+			
 		}
 	} else {
 		return fromDisplay;
@@ -438,7 +465,7 @@ function squareRoot (fromDisplay) {
 
 function square (fromDisplay) {
 	if (fromDisplay.includes('^2')) {
-		let squareCount = (fromDisplay.match(/^/g) || []).length;
+		let squareCount = (fromDisplay.match(/\^/g) || []).length;
 		let equalToCount = squareCount;
 		for (i=0; i < squareCount; i++) {
 			if (equalToCount < 2) {
@@ -447,23 +474,97 @@ function square (fromDisplay) {
 				let toBeSquared = fromDisplay.slice(0, squareIndex);
 				console.log(toBeSquared);
 				squared = toBeSquared ** 2;
-				fromDisplay = squared;
-				displayText.textContent = squared;
-				console.log(fromDisplay);
-				return fromDisplay;
-			} /*else {
-				console.log('adding more than two numbers');
-				additionIndex = fromDisplay.indexOf('+');
-				nextAdditionIndex = fromDisplay.indexOf('+', additionIndex + 1);
-				let num1 = fromDisplay.slice(0, additionIndex);
-				let num2 = fromDisplay.slice(additionIndex + 1, nextAdditionIndex)
-				let added = Number(num1) + Number(num2);
-				console.log('added ' + added);
-				fromDisplay = added + fromDisplay.slice(nextAdditionIndex);
-				console.log('fromDis ' + fromDisplay);
+				console.log(squared);
+				if (fromDisplay.includes('+' || '-' || '*' || '√' || '÷' )) {
+					let withoutSquared = fromDisplay = fromDisplay.slice(twoIndex + 1);
+					let firstChar = fromDisplay.charAt(0);
+					console.log('nosquared' + withoutSquared);
+					if (!(firstChar >= 0 && firstChar <= 9)) {
+						moveToBack = fromDisplay[0];
+						fromDisplay = fromDisplay.slice(1);
+						fromDisplay = fromDisplay + moveToBack + squared;
+						console.log('fromDis ' + fromDisplay);
+						return fromDisplay.toString();
+					}
+				} else {
+					fromDisplay = squared;
+					displayText.textContent = squared;
+					console.log(fromDisplay);
+					return fromDisplay.toString();
+				}
+				
+			} else {	
+				console.log('squaring more than two numbers');
+				squareIndex = fromDisplay.indexOf('^');
+				twoIndex = fromDisplay.indexOf('2', squareIndex + 1);
+				toBeSquared = fromDisplay.slice(0, squareIndex);
+				squared = toBeSquared ** 2;
+				fromDisplay = fromDisplay.slice(twoIndex + 1);
+				firstChar = fromDisplay.charAt(0);
+				if (firstChar >= 0 && firstChar <= 9) {
+					fromDisplay = fromDisplay.slice(twoIndex + 1) + squared;
+					console.log('fromDis ' + fromDisplay);
+				} else {
+					moveToBack = fromDisplay[0];
+					fromDisplay = fromDisplay.slice(1);
+					fromDisplay = fromDisplay + moveToBack + squared;
+					console.log('fromDis ' + fromDisplay);
+				}
 				equalToCount--;
-			}*/
+			}
 		}
+	} else {
+		return fromDisplay;
+	}
+}
+
+function multiply (fromDisplay) {
+	if (fromDisplay.includes('*')) {
+		console.log('has multiply sign');
+		let multiplyCount = (fromDisplay.match(/\*/g) || []).length;
+		let equalToCount = multiplyCount;
+		for (i=0; i < multiplyCount; i++) {
+			if (equalToCount < 2) {
+				console.log('just multiplying one');
+				let multiplyIndex = fromDisplay.indexOf('*');
+				let num1 = fromDisplay.slice(0, multiplyIndex);
+				let num2 = fromDisplay.slice(multiplyIndex + 1);
+				multiplied = num1 * num2;
+				console.log(multiplied);
+				if (fromDisplay.includes('+' || '-' || '^' || '√' || '÷' )) {
+					let withoutSquared = fromDisplay = fromDisplay.slice(twoIndex + 1);
+					let firstChar = fromDisplay.charAt(0);
+					if (!(firstChar >= 0 && firstChar <= 9)) {
+						moveToBack = fromDisplay[0];
+						fromDisplay = fromDisplay.slice(1);
+						fromDisplay = fromDisplay + moveToBack + squared;
+						console.log('fromDis ' + fromDisplay);
+						return fromDisplay.toString();
+					}
+				} else {
+					fromDisplay = multiplied;
+					displayText.textContent = multiplied;
+					console.log(fromDisplay);
+					return fromDisplay.toString();
+				}
+				
+			} else {	
+				console.log('multiplying more than two numbers');
+				multiplyIndex = fromDisplay.indexOf('*');
+				nextIndex = fromDisplay.indexOf('*', multiplyIndex + 1);
+				num1 = fromDisplay.slice(0, multiplyIndex);
+				num2 = fromDisplay.slice(multiplyIndex + 1, nextIndex);
+				console.log(num1 + '' + num2);
+				multiplied = num1 * num2;
+				console.log(multiplied);
+				fromDisplay = multiplied + '*' + fromDisplay.slice(nextIndex + 1);
+				console.log('from display: ' + fromDisplay);
+				
+			equalToCount--;
+			}
+		} 
+
+		return fromDisplay.toString(); 
 	} else {
 		return fromDisplay;
 	}
@@ -495,9 +596,9 @@ function add (fromDisplay) {
 				fromDisplay = added + fromDisplay.slice(nextAdditionIndex);
 				console.log('fromDis ' + fromDisplay);
 				equalToCount--;
-			}
+			} 
 		}
-		return fromDisplay; 
+		return fromDisplay.toString(); 
 	} else {
 		return fromDisplay;
 	}
@@ -541,12 +642,15 @@ function executionContinued (input) {
 	let fromDisplay = input;
 	let i = 0;
 	let fromParenthesis = [];
-	fromDisplay = square(fromDisplay);
-	console.log('after sqr ' + fromDisplay);
+	
 	fromDisplay = squareRoot(fromDisplay);
 	console.log('after sqrrt ' + fromDisplay);
-	
 
+	fromDisplay = square(fromDisplay);
+	console.log('after sqr ' + fromDisplay);
+	
+	fromDisplay = multiply(fromDisplay);
+	console.log('after multiply ' + fromDisplay);
 	fromDisplay = add(fromDisplay);
 	console.log('after add ' + fromDisplay);
 	fromDisplay = subtract(fromDisplay);
@@ -687,7 +791,7 @@ function subtract () {
 }*/
 
 //Needs to multiply
-function multiply (array) {
+/*function multiply (array) {
 	let arrayLength = array.length;
 	let value = 1;
 	for (i = 0; (i < arrayLength); i++) {
@@ -695,7 +799,7 @@ function multiply (array) {
 	}
 	
 	return value;
-}
+}*/
 
 /*//Squares a number
 function square() {
